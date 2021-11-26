e289eda2b410dae7e7d2ca2e4891a821c25ba9e4
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2021-11-26 17:34:08 -0500

    Update Camioneta.java

--- src/main/java/Camioneta.java
+++ src/main/java/Camioneta.java
@@ -7,7 +7,7 @@ public class Camioneta extends Vehiculo {
 	public Camioneta(String placa, int puertas, String nombre, double precio, double peso, Fabricante fabricante, boolean volco) {
 		super(placa, puertas, 80, nombre, precio, peso, "4X4", fabricante);
 		camionetas++;
-		volco = this.volco;
+		this.volco = volco;
 	}
 
 	public boolean isVolco() {
